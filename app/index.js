748ce36b40b4e8961a255c47877f640013db2b77
Damien <damien@localhost.localdomain>
2017-05-17 18:41:29 +0200

    is friend isnotfriend works

--- app/index.js
+++ app/index.js
@@ -1,20 +1,60 @@
-var React = require ('react');
+var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types')
 require('./index.css');
 
 
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello you !
-      </div>
-    )
-  }
+class Users extends React.Component {
+    render() {
+
+        let isFriend = this.props.list.filter(function(isFriend) {
+            return isFriend.friend === true;
+        })
+
+        let isNotFriend = this.props.list.filter(function(isNotFriend) {
+            return isNotFriend.friend === false;
+        })
+
+        return ( 
+          <div>
+            <h1>Friends</h1> 
+            <ul> 
+                {isFriend.map(function(isFriend) {
+                    return <li key={isFriend.name}>{isFriend.name}</li>
+                })} 
+            </ul>
+
+            <hr/>
+
+            <h1>Non Friends</h1> 
+            <ul>
+                {isNotFriend.map(function(isNotFriend) {
+                    return <li key={isNotFriend.name}>{isNotFriend.name}</li>
+                })} 
+            </ul>        
+            </div>
+        )
+    }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+
+Users.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      friend: PropTypes.bool.isRequired,
+  }))
+}
+
+
+ReactDOM.render( 
+  <Users list = {[
+            { name: 'Tyler', friend: true },
+            { name: 'Ryan', friend: true },
+            { name: 'Michael', friend: false },
+            { name: 'Mikenzi', friend: false },
+            { name: 'Jessica', friend: true },
+            { name: 'Dan', friend: false } ]}
+        />,
+    document.getElementById('app')
 );
