8800160e280278cd89f1a74b80a687bdab1fa85c
Damien <jdamien@nobatek.com>
2017-06-23 15:27:33 +0200

    can now fetsh popular repo

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import api from '../utils/api'
 
 function SelectLanguage(props) {
   let languages = ['All', 'Javascript', 'Ruby', 'Java', 'Css', 'Python'];
   return (
       <ul className="languages">
         {languages.map(function(lang) {
-          return (          
+          return (
             <li
             style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
               onClick={props.onSelect.bind(null, lang)}
@@ -28,15 +29,28 @@ export class Popular extends React.Component {
     constructor (props) {
       super(props);
       this.state = {
-        selectedLanguage: 'All'
+        selectedLanguage: 'All',
+        repos: null
       };
       this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    componentDidMount() {
+      this.updateLanguage(this.state.selectedLanguage)
     }
 
     updateLanguage(lang) {
       this.setState({
         selectedLanguage: lang
       })
+      api.fetchPopularRepos(lang)
+        .then(function(repos) {
+          this.setState(function () {
+            return {
+              repos: repos
+            }
+          })
+        }.bind(this))
     }
 
     render() {
@@ -44,11 +58,9 @@ export class Popular extends React.Component {
           <div>
             <SelectLanguage
               selectedLanguage={this.state.selectedLanguage}
-              onSelect={this.updateLanguage} 
+              onSelect={this.updateLanguage}
               />
           </div>
       )
     }
   }
-
-
